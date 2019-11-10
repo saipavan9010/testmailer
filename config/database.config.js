cca68adddd832d815833c62ece8b9bf8d6669eba
saipavan <saipavan4739@gmail.com>
2019-11-10 18:51:41 +0530

    Db Changes

--- config/database.config.js
+++ config/database.config.js
@@ -1,7 +1,7 @@
-// module.exports = {
-//   url: 'mongodb+srv://admin:admin@testmailer-qofp2.mongodb.net/test?retryWrites=true&w=majority'
-// }
-
 module.exports = {
-  url: 'mongodb://localhost:27017/testmailer'
+  url: 'mongodb+srv://admin:admin@testmailer-qofp2.mongodb.net/test?retryWrites=true&w=majority'
 }
+
+// module.exports = {
+//   url: 'mongodb://localhost:27017/testmailer'
+// }
